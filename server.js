720b5d560d67129be2306c7e76390852cbfb6136
Glitch (tan-balaur) <none>
2019-08-25 17:17:19 +0000

    🍘🛍 Checkpoint ./server.js:6079345/58

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/whoami", function (req, res) {
-  res.json({IP: req.query.whoami});
+  res.json({IP:'https://api.ipify.org?format=json'});
 });
 
 
